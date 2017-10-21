09c2b89bb7c91ec79ba6b4d45c951a204585b9cf
wytriam <thomas.m.frick@gmail.com>
2017-10-21 17:45:57 -0400

    Prepping to make projectiles work

---
+++ Tower Defense/Assets/Scripts/Projectile_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Manager : MonoBehaviour
+{
+    public float maxHeight; //after reach this height, the projectile finds the nearest enemy and attacks it
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Tower Defense/Assets/Scripts/Tower_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_Manager : MonoBehaviour
+{
+    public GameObject projectile;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+}
