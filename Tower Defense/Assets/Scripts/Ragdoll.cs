d388634e70abb7e7332bfad1ddcba57db23d0b61
Seanboyy <sbamford12@gmail.com>
2017-10-23 13:28:33 -0400

    Fixed merge commits

--- Tower Defense/Assets/Scripts/Ragdoll.cs
+++ Tower Defense/Assets/Scripts/Ragdoll.cs
@@ -11,6 +11,7 @@ public class Ragdoll : Enemy_Manager {
     private Quaternion uprightRotation, rootNeutralRot, leftHipNeutralRot, rightHipNeutralRot, leftKneeNeutralRot, rightKneeNeutralRot, leftShoulderNeutralRot, rightShoulderNeutralRot, leftElbowNeutralRot, rightElbowNeutralRot, midSpineNeutralRot, neckNeutralRot;
     private float counter;
     private int foot = 0;
+    private bool right = false;
 
     void Awake(){
         uprightRotation = transform.rotation;
@@ -47,9 +48,16 @@ public class Ragdoll : Enemy_Manager {
 	void Update () {
         counter += Time.deltaTime;
         if(counter >= walkCycleSpeed){
-            if (foot == 0) foot++;
-            else if (foot == 1) foot -= 2;
-            else if (foot == -1) foot++;
+            if (foot == 0 && right) foot--;
+            else if (foot == 0 && !right) foot++;
+            else if (foot == -1){
+                foot++;
+                right = false;
+            }
+            else if(foot == 1){
+                foot--;
+                right = true;
+            }
             Walk(foot);
             counter = 0.0F;
         }
@@ -88,10 +96,12 @@ public class Ragdoll : Enemy_Manager {
     }
 
     void StepRight(){
-        rightHip.transform.localRotation *= Quaternion.Euler(0, 0, -1 * Time.deltaTime);
+        rightHip.gameObject.transform.localRotation *= Quaternion.Euler(-1 * Time.deltaTime, 0, 0);
+        rightHip.gameObject.transform.position += Vector3.forward * Time.deltaTime;
     }
 
     void StepLeft(){
-
+        leftHip.gameObject.transform.localRotation *= Quaternion.Euler(1 * Time.deltaTime, 0, 0);
+        leftHip.gameObject.transform.position += Vector3.forward * Time.deltaTime;
     }
 }
