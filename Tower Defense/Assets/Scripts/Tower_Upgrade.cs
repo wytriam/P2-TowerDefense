c295a35d1d950747aefb168e88157168bb555681
wytriam <thomas.m.frick@gmail.com>
2017-10-24 12:18:05 -0400

    upgradable towers (prefabs set up)
    There is a known bug: if you upgrade a tower while an enemy is nearby, those enemies will become immortal.

---
+++ Tower Defense/Assets/Scripts/Tower_Upgrade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_Upgrade : MonoBehaviour
+{
+    public GameObject upgradesToPrefab;
+    public float costToUpgrade = 100f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        upgrade();
+    }
+
+    public void upgrade()
+    {
+        // create the new tower based on the prefab and set it's transform to be equivalent to this transform
+        GameObject upgrade = Instantiate(upgradesToPrefab);
+        upgrade.transform.position = gameObject.transform.position;
+        upgrade.transform.rotation = gameObject.transform.rotation;
+        upgrade.transform.localScale = gameObject.transform.localScale;
+        // delete the old tower
+        Destroy(gameObject);
+
+    }
+}
