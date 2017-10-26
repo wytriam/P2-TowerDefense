5e3e9cbd673a59dc64a5569dcda9429bba1f6772
Seanboyy <sbamford12@gmail.com>
2017-10-25 22:18:27 -0400

    Uhhh......

--- Tower Defense/Assets/Scripts/SM_level_1.cs
+++ Tower Defense/Assets/Scripts/SM_level_1.cs
@@ -11,29 +11,12 @@ public class SM_level_1 : SM_staging
 
     void Start()
     {
-<<<<<<< HEAD
-=======
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;
-        spawnPoint = enemySpawnPoint.GetComponent<WytriamSTD.Spawn>();
-        spawnEnemy();
->>>>>>> bc3723686ddfee6b41a934c4f29d2b62366e36c0
         mana = GetComponent<Mana>();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        if (Input.GetKeyDown("escape"))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Confined;
-            Cursor.visible = true;
-        }
         manaDisplay.text = "Mana: " + mana.currentMana.ToString("F2");
 	}
 }
