86640caad84e46bec852a8359c8a9885c36c402c
Seanboyy <sbamford12@gmail.com>
2017-10-25 23:06:35 -0400

    Changed terrain, added nightlight

--- Tower Defense/Assets/WytriamSTD/Scripts/DayNightCycle.cs
+++ Tower Defense/Assets/WytriamSTD/Scripts/DayNightCycle.cs
@@ -6,21 +6,41 @@ namespace WytriamSTD
 {
     public class DayNightCycle : MonoBehaviour
     {
-
+        public Light[] sceneLights;
         public Light sun;
         private Quaternion sunRotation;
+        public float dayLengthMultiplier;
         public float speed;
 
         // Use this for initialization
         void Start()
         {
             sunRotation = sun.transform.rotation;
+            foreach(Light light in sceneLights)
+            {
+                light.intensity = 0;
+            }
         }
 
         void FixedUpdate()
         {
             sun.transform.rotation = sunRotation;
-            sunRotation *= Quaternion.Euler(speed * Time.fixedDeltaTime, 0, 0);
+            if(sunRotation.eulerAngles.x >= 180)
+            {
+                sunRotation *= Quaternion.Euler(speed * (1 + dayLengthMultiplier) * Time.fixedDeltaTime, 0, 0);
+                foreach (Light light in sceneLights)
+                {
+                    if (light.intensity < 1) light.intensity += Time.fixedDeltaTime;
+                }
+            }
+            if(sunRotation.eulerAngles.x < 180)
+            {
+                sunRotation *= Quaternion.Euler(speed * (1 - dayLengthMultiplier) * Time.fixedDeltaTime, 0, 0);
+                foreach (Light light in sceneLights)
+                {
+                    if (light.intensity > 0) light.intensity -= Time.fixedDeltaTime;
+                }
+            }
         }
     }
 }
