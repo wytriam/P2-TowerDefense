9a8b67077ba809bde16f17c5b95c8c5bffb45bc9
wytriam <thomas.m.frick@gmail.com>
2017-11-02 20:29:40 -0400

    starting to add effects for tower variants

---
+++ Tower Defense/Assets/Scripts/Effect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    public bool damageOverTime;
+    public float DoTdamage = 1;
+    public bool slowing;
+    public float effectDuration = 5f;
+
+    [HideInInspector]
+    public Enemy_Manager enemy;
+    private float timer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        enemy = this.transform.root.gameObject.GetComponent<Enemy_Manager>();
+        if (enemy == null)
+        {
+            Debug.Log("Not attached to an enemey");
+            Destroy(gameObject);
+        }
+
+        StartCoroutine("effect");
+	}
+	
+    IEnumerator effect()
+    {
+        if (slowing)
+            enemy.speed /= 2;
+        while (true)
+        {
+            // put effect here
+            if (damageOverTime)
+            {
+                enemy.health -= DoTdamage;
+            }
+            timer += 1;
+            if (timer > effectDuration)
+                Destroy(gameObject);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
