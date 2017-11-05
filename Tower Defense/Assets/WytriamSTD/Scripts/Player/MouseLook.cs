7c8c74742cbf13c1f8347b3fe1ef79f9fa3215fc
wytriam <thomas.m.frick@gmail.com>
2017-11-05 16:56:56 -0500

    updated mouselook. so it sucks.

--- Tower Defense/Assets/WytriamSTD/Scripts/Player/MouseLook.cs
+++ Tower Defense/Assets/WytriamSTD/Scripts/Player/MouseLook.cs
@@ -19,6 +19,7 @@ using System.Collections;
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public class MouseLook : MonoBehaviour
 {
+    public bool lockCameraWithMouse = false;
 
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
@@ -35,7 +36,30 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse1))   //only move the camera if the right mouse button is being held. 
+        if (lockCameraWithMouse && Input.GetKey(KeyCode.Mouse1))   //only move the camera if the right mouse button is being held. 
+        {
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+            }
+        }
+        else
         {
             if (axes == RotationAxes.MouseXAndY)
             {
